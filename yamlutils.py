44e1c1837d8f2cf7176c20a425bb98ca3955077b
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2015-05-20 22:55:43 +0200

    simplified SafeOrderedDumper

--- yamlutils.py
+++ yamlutils.py
@@ -2,8 +2,8 @@ import yaml
 from collections import OrderedDict
 
 class SafeOrderedDumper(yaml.SafeDumper):
-    """ 
-    A Yaml dumper which is able to store OrderedDicts
+    """
+    A Yaml dumper which is able to serialize OrderedDicts
 
     Usage: `yaml.dump(data, Dumper=SafeOrderedDumper)`
     """
@@ -14,26 +14,6 @@ class SafeOrderedDumper(yaml.SafeDumper):
         super(SafeOrderedDumper, self).__init__(*args, **kwargs)
         self.add_representer(OrderedDict, self.represent_odict)
 
-    def represent_odict(self, dump, mapping, flow_style=None):
+    def represent_odict(self, _, mapping):
         "Like BaseRepresenter.represent_mapping, but does not issue the sort()."
-        value = []
-        node = yaml.MappingNode(self._odict_tag, value, flow_style=flow_style)
-        if dump.alias_key is not None:
-            dump.represented_objects[dump.alias_key] = node
-        best_style = True
-        if hasattr(mapping, 'items'):
-            mapping = mapping.items()
-        for item_key, item_value in mapping:
-            node_key = dump.represent_data(item_key)
-            node_value = dump.represent_data(item_value)
-            if not (isinstance(node_key, yaml.ScalarNode) and not node_key.style):
-                best_style = False
-            if not (isinstance(node_value, yaml.ScalarNode) and not node_value.style):
-                best_style = False
-            value.append((node_key, node_value))
-        if flow_style is None:
-            if dump.default_flow_style is not None:
-                node.flow_style = dump.default_flow_style
-            else:
-                node.flow_style = best_style
-        return node
+        return self.represent_mapping(self._odict_tag, mapping.items());

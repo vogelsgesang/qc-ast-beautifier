15a4691c035a203cf767982248091bf3ec5f02d1
Adrian Vogelsgesang <adrian.vogelsgesang@tum.de>
2015-09-10 01:55:06 +0200

    do not combine number/string/None nodes

--- astvisualizer.py
+++ astvisualizer.py
@@ -3,23 +3,29 @@ import yaml
 import inspect
 import graphviz as gv
 import subprocess
+import numbers
+from uuid import uuid4 as uuid
 
 class GraphRenderer:
     """
-    this class is capable of rendering arbitrary data structures as a graph using graphviz
+    this class is capable of rendering data structures consisting mostly of
+    dicts and lists as a graph using graphviz
     """
+
     graphattrs = {
         'labelloc': 't',
         'fontcolor': 'white',
         'bgcolor': '#333333',
         'margin': '0',
     }
+
     nodeattrs = {
         'color': 'white',
         'fontcolor': 'white',
         'style': 'filled',
         'fillcolor': '#006699',
     }
+
     edgeattrs = {
         'color': 'white',
         'fontcolor': 'white',
@@ -28,29 +34,46 @@ class GraphRenderer:
     _graph = None
     _rendered_nodes = None
 
+
     @staticmethod
     def _escape_dot_label(str):
         return str.replace("\\", "\\\\").replace("|", "\\|")
 
+
     def _render_node(self, node):
-        if id(node) in self._rendered_nodes:
-            return
-        self._rendered_nodes.add(id(node))
-        node_id = node_id
-        graph = self._graph
-        if isinstance(node, dict):
-            graph.node(node_id, label=node.get("node_type"))
-            for key, value in node.items():
-                if key == "node_type": continue
-                self._render_node(value)
-                graph.edge(node_id, str(id(value)), label=key)
-        elif isinstance(node, list):
-            graph.node(node_id, label="[list]")
-            for idx, value in enumerate(node):
-                self._render_node(value)
-                graph.edge(node_id, str(id(value)), label=str(idx))
+        if isinstance(node, (str, numbers.Number)) or node is None:
+            node_id = uuid()
         else:
-            graph.node(node_id, label=str(node))
+            node_id = id(node)
+        node_id = str(node_id)
+
+        if node_id not in self._rendered_nodes:
+            self._rendered_nodes.add(node_id)
+            if isinstance(node, dict):
+                self._render_dict(node, node_id)
+            elif isinstance(node, list):
+                self._render_list(node, node_id)
+            else:
+                self._graph.node(node_id, label=str(node))
+
+        return node_id
+
+
+    def _render_dict(self, node, node_id):
+        self._graph.node(node_id, label=node.get("node_type", "[dict]"))
+        for key, value in node.items():
+            if key == "node_type":
+                continue
+            child_node_id = self._render_node(value)
+            self._graph.edge(node_id, child_node_id, label=key)
+
+
+    def _render_list(self, node, node_id):
+        self._graph.node(node_id, label="[list]")
+        for idx, value in enumerate(node):
+            child_node_id = self._render_node(value)
+            self._graph.edge(node_id, child_node_id, label=str(idx))
+
 
     def render(self, data, *, label=None):
         # create the graph
@@ -70,6 +93,7 @@ class GraphRenderer:
         graph.format = "pdf"
         graph.render("test")
         subprocess.Popen(['xdg-open', "test.pdf"])
+
 
 if __name__ == '__main__':
     import sys
